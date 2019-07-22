ef108dd6d0a9d9a060d13085e0b5bca1bbd26e4c
cwksean10596 <x>
2019-07-22 11:10:12 -0700

    I added exercises

---
+++ Excercises.py
@@ -0,0 +1,43 @@
+def main ():
+
+    variable = input ("What is life")
+    print (variable)
+
+
+def two ():
+
+    y = 10
+
+    print (y)
+
+def three ():
+
+    x = input ("Type in a number")
+    i = int (x)
+
+    if i == 10:
+       print ("This is 10")
+
+    if i > 10:
+            print ("THis is greater than 10")
+
+    if i < 10:
+            print ("This is less than 10")
+
+def four ():
+    o = input ("type in a number")
+    y = int (o)
+    y%2
+    if y%2 == 1:
+        print ("Odd number")
+
+    if y%2 == 0:
+        print ("Even number")
+
+
+
+
+
+if __name__ == "__main__":
+    four()
+
